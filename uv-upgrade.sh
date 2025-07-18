74e29889b2972bb1a1acc28d9c6b0da08b2b5652
Anirban Basu <anirbanbasu@users.noreply.github.com>
2025-07-18 11:44:17 +0900

    fix: A possible fix to the exported requirements.txt for HF Spaces deployment.

--- uv-upgrade.sh
+++ uv-upgrade.sh
@@ -2,6 +2,6 @@
 # Upgrade the dependencies including those from Git sources
 uv lock -U
 # Export the requirements to a requirements.txt file
-uv export --no-dev --locked --no-hashes --format requirements-txt > requirements.txt
+uv export --no-editable --no-annotate --no-emit-workspace --no-dev --frozen --no-hashes --format requirements-txt > requirements.txt
 # Run the tests and report coverage with missing values
 uv run --group test pytest tests/
